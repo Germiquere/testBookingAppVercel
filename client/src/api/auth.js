a632ee1010f305a2399c85b8590f90d84d331b33
Germiquere <german150703.miquere@gmail.com>
2023-12-02 14:40:33 -0300

    cambiando los endpoints de login para hacer la prueba

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,6 +1,6 @@
 export const login = async (user) => {
     try {
-        const res = await fetch(`http://localhost:8080/auth/login`, {
+        const res = await fetch(`http://54.227.169.64:8080/auth/login`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -32,7 +32,7 @@ export const register = async (user) => {
     console.log("estoy pro registrar");
 
     try {
-        const res = await fetch(`http://localhost:8080/auth/registrar`, {
+        const res = await fetch(`http://54.227.169.64:8080/auth/login`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
